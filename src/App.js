a883fd9454f8887acbe37b985583330ede6e2734
Dajana <djnfrs255@gmail.com>
2021-09-01 14:15:25 +0200

    search engine

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-
 import './App.css';
-
+import Dictionary from "./Dictionary"
 function App() {
   return (
     <div className="App">
       <h1> Dictonary App</h1>
       <header className ="App-header">
       </header>
+      <main>
+        <Dictionary/>
+      </main>
     </div>
   );
 }
