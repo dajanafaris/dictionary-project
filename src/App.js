003ef9bd7846a150fec4a99fa68c15ea7db9e1ce
Dajana <djnfrs255@gmail.com>
2021-09-01 11:41:07 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Dictonary App</h1>
+      <header className ="App-header">
+      </header>
+    </div>
+  );
+}
+
+export default App;
