f0e57901720d0dc9a2938c301f15bad07bc9dd3b
Dajana <djnfrs255@gmail.com>
2021-09-02 14:07:58 +0200

    meaning component

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,21 +1,21 @@
 import React from "react";
-import Meaning from "./Meaning";
 
-export default function Results(props) {
-  if (props.results) {
-    return (
-      <div className="Results">
-        <h2>{props.results.word}</h2>
-        {props.results.meanings.map(function (meaning, index) {
-          return (
-            <div key={index}>
-              <Meaning meaning={meaning} />
-            </div>
-          );
-        })}
-      </div>
-    );
-  } else {
-    return null;
-  }
+export default function Meaning(props) {
+  console.log(props.meaning);
+  return (
+    <div className="Meaning">
+      <h3>{props.meaning.partOfSpeech}</h3>
+      {props.meaning.definitions.map(function (definition, index) {
+        return (
+          <div key={index}>
+            <p>
+              {definition.definition}
+              <br />
+              <em>{definition.example}</em>
+            </p>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
