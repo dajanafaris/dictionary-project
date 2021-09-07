d2faa7eafa459a3eaa0e06ad0cc7bfc9624e0a1a
Dajana <djnfrs255@gmail.com>
2021-09-07 14:48:09 +0200

    Dictionary css

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import './App.css';
-import Dictionary from "./Dictionary"
+import "./App.css";
+import Dictionary from "./Dictionary";
 function App() {
   return (
     <div className="App">
       <h1> Dictonary App</h1>
-      <header className ="App-header">
-      </header>
+      <header className="App-header"></header>
       <main>
-        <Dictionary/>
+        <Dictionary defaultKeyword="sun" />
       </main>
+      <footer className="App-footer">
+        <small>Coded by Dajana Faris</small>
+      </footer>
     </div>
   );
 }
